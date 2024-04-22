2a6d3fb52e16f33302b16fcb0f0e195300597f53
Quadir <aquadirmuhammad@gmail.com>
2024-04-22 15:22:02 +0100

    glide css imported

--- components/Carousel/Tokenomics.vue
+++ components/Carousel/Tokenomics.vue
@@ -50,7 +50,8 @@
       </div>
 </template>
 <script>
-import Glide from '@glidejs/glide'
+import Glide from '@glidejs/glide',
+import '@glidejs/glide/dist/css/glide.core.min.css';
 export default {
   data(){
     return {
